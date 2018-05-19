3c3c3ec0d391121e624f92e70ed1dbdb0875f825
Jared Jensen <jared@forgerock.com>
2018-05-18 18:04:34 -0700

    Updated default AM url

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export OAUTH_ISSUER=http://openam.house-of-gordon-am.forgeblocks.com/openam/oauth2
+export OAUTH_ISSUER=http://openam.fr-saas-hog.forgeblocks.com/openam/oauth2
 export OAUTH_KEY=$1
 export OAUTH_SCOPES="openid profile"
 export OAUTH_SECRET=$2
