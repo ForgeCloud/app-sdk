e665a1b0488f14d2e53f9c314b288b506c11f771
Lee Owen <ItsLeeOwen@users.noreply.github.com>
2018-12-07 15:58:51 -0500

    non-hosted signin (#16)

---
+++ server/config.js
@@ -0,0 +1,32 @@
+const {
+  ORG_GATEWAY_URL = 'http://localhost:8086',
+  HOST = 'app.example.com',
+  OAUTH_ISSUER = 'https://openam-example.com/openam/oauth2',
+  OAUTH_KEY,
+  OAUTH_SCOPES = 'openid',
+  OAUTH_SECRET,
+  PORT = 9080,
+  PROTOCOL = 'http',
+  TENANT = '',
+} = process.env;
+
+const BASE_URL = `${PROTOCOL}://${HOST}${PORT == 80 ? '' : `:${PORT}`}`;
+const CALLBACK_HOSTED = `${BASE_URL}/callback`;
+const CALLBACK_NON_HOSTED = `${BASE_URL}/callback/non-hosted`;
+const OPEN_AM = `https://openam-${TENANT}.forgeblocks.com/openam`;
+
+module.exports = {
+  BASE_URL,
+  CALLBACK_HOSTED,
+  CALLBACK_NON_HOSTED,
+  ORG_GATEWAY_URL,
+  HOST,
+  OAUTH_ISSUER,
+  OAUTH_KEY,
+  OAUTH_SCOPES,
+  OAUTH_SECRET,
+  OPEN_AM,
+  PORT,
+  PROTOCOL,
+  TENANT,
+};
