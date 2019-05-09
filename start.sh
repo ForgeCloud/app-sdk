f60532275a7ae6efe2f7f72ee27a71bef13968c0
Jared Jensen <jared.jensen@forgerock.com>
2019-05-09 10:55:56 -0700

    Made ID token alg configurable (#22)

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
-export TENANT=$1
 export CLIENT_ID=$2
 export CLIENT_SECRET=$3
-export ORG_GATEWAY_URL=https://api-$1.forgeblocks.com
 export HOST=localhost
+export ID_TOKEN_SIGNING_ALGORITHM=$4
 export OAUTH_ISSUER=https://openam-$1.forgeblocks.com/oauth2
 export OAUTH_SCOPES="openid me.read"
+export ORG_GATEWAY_URL=https://api-$1.forgeblocks.com
+export TENANT=$1
 
 echo Starting sample application...
 npm start
